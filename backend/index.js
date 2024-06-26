64edab6e94394e7098bb2ce5558c913d1d95bf59
keylat160 <keylat160@gmail.com>
2024-06-26 14:37:46 -0400

    Added react to frontend and migration folder

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,33 @@
 const express = require('express');
 const app = express();
+const port =3000
+const { sequelize } = require('./models')
 
-//app set
+app.use(express.json())
+
+
+//server front-end
 
 app.get('/', (req, res) => {
-    res.send(('home'));
+    //send my front-end
 })
 
 //controllers
 app.use('/recipes', require('./controllers/recipes'));
+
+//port
+app.listen(port, () => {
+    console.log(`server is running on port ${port}`);
+    console.log(`Database host: ${dbHost}`);
+});
+
+//user table
+//name, email, password
+
+//recipes
+//Chicken in cream of mushrooms 
+//Green pasta
+//Orange chicken
+//Beef mexican stew
+
+//ingredients
