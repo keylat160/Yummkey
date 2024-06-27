db98ea0da44d3ff7789616b2f4e379e8af0f49b4
keylat160 <keylat160@gmail.com>
2024-06-27 17:04:39 -0400

    adding bootstrap and react router

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import { Home } from './pages'
+
 import App from './App.tsx'
 import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import { Navigation } from "../components"
+
+const Home = () => {
+   return (
+       <>
+     <main className="container">
+        <h1>Home</h1>
+    </main>
+    </>
+  )
+}
+
+export default Home
