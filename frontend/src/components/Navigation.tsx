47741f1b2726ce8458147e9040602a064f334bfa
keylat160 <keylat160@gmail.com>
2024-06-28 15:01:17 -0400

    added home animation

--- frontend/src/components/Navigation.tsx
+++ frontend/src/components/Navigation.tsx
@@ -18,7 +18,7 @@ const Navigation = () => {
 
    const [userData, setUserData] = userContext;
 
-   
+
   const handleLogout = () => {
     console.log("logout");
     if (setUserData)
@@ -35,7 +35,7 @@ const Navigation = () => {
     <>
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home">Home</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -10,7 +10,23 @@ const Home = () => {
        <>
        <Navigation />
      <main className="container">
-        <h1>Welcome to Yummkey</h1>
+     <div className="patterns">
+  <svg width="100%" height="100%">
+    <defs>
+      <pattern id="polka-dots" x="0" y="0"                    width="100" height="100"
+               patternUnits="userSpaceOnUse">
+        <circle fill="#be9ddf" cx="25" cy="25" r="3"></circle>
+      </pattern>  
+        <style>
+     @import url("https://fonts.googleapis.com/css?  family=Lora:400,400i,700,700i");
+   </style>
+      </defs>
+    <rect x="0" y="0" width="100%" height="100%" fill="url(#polka-dots)"> </rect>
+     <text x="50%" y="60%"  text-anchor="middle"  >
+   Yummkey
+ </text>
+ </svg>
+</div>
         <img src='https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
         alt='food pic from pexels'/>
         {userData.id ? <Recipes /> : null}
